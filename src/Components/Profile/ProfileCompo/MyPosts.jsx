ced74978bff31f06e1127071e0ad00bdfa73f7fe
Zheka113 <zhekasenko@gmail.com>
2021-10-11 18:07:10 +0300

    MySite-v2.3.1

--- src/Components/Profile/ProfileCompo/MyPosts.jsx
+++ src/Components/Profile/ProfileCompo/MyPosts.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import postsStyle from './myposts.module.css';
 
+function Addpost(){
+    console.log('gg')
+}
+
 const MyPosts = () => {
     return (
         <div className={postsStyle.posts}>
                 <h3>My Posts</h3>
                 <textarea></textarea>
                 <br/>
-                <button>Send</button>
+                <button onClick={Addpost()}>Send</button>
         </div>
     );
 }
